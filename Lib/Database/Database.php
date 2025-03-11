c8d7cce47593197eb6872e4237737eb2647b5d92
ndnestor <trabajo.nestor.098@gmail.com>
2025-03-11 17:09:32 +0100

    database is running

--- Lib/Database/Database.php
+++ Lib/Database/Database.php
@@ -2,14 +2,50 @@
 
 namespace Database;
 
+use Exception;
+use mysqli;
+
 class Database {
     protected static $conn;
+    protected $server;
+    protected $user;
+    protected $password;
+    protected $database;
+    protected $port;
 
     public function __construct() {
-        $this->conn = "Hola";
+        $this->server = getenv("DB_HOST");
+        $this->user = getenv("DB_USERNAME");
+        $this->password = getenv("DB_PASSWORD");
+        $this->database = getenv("DB_DATABASE");
+        $this->port = getenv("DB_PORT");
+
+        if (!isset(self::$conn)) {
+            self::$conn = new mysqli($this->server, $this->user, $this->password, $this->database, $this->port);
+
+            // Verificar errores de conexión
+            if (self::$conn->connect_error) {
+                throw new Exception("Connection error: " . self::$conn->connect_error);
+            }
+        }
     }
 
-    public static function getConn(){
-        echo self::$conn;
+    public function __destruct() {
+        if (isset(self::$conn)) {
+            self::$conn->close();
+        }
     }
+
+    public function conn(){
+        return self::$conn;
+    }
+
+    public function config(){
+        return "Connection: " . getenv("DB_CONNECTION") .
+               " <br>Server Name: " . getenv("DB_HOST") . 
+               " <br>Port: " . getenv("DB_PORT") .
+               " <br>Database: " . getenv("DB_DATABASE") .
+               " <br>User Name: " . getenv("DB_USERNAME") . 
+               " <br>Password: " . getenv("DB_PASSWORD");
+    }    
 }
--- Public/index.php
+++ Public/index.php
@@ -1,4 +1,18 @@
 <?php
 
-require_once "../vendor/autoload.php";
+use Database\Database;
 
+require_once "../Lib/autoload.php";
+require_once "../Lib/env.php";
+
+$database = new Database;
+
+$conn = $database->conn();
+
+if($conn){
+    $query = $conn->query("SELECT * FROM users");
+
+    foreach ($query as $value) {
+        echo $value['name'];
+    }
+}
