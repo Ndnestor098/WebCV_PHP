9602fd26c5443eb2609fb47063e3052bfa15f753
ndnestor <trabajo.nestor.098@gmail.com>
2025-04-05 14:11:11 +0200

    test

--- Public/index.php
+++ Public/index.php
@@ -1,10 +1,10 @@
 <?php
+echo "Hello World!";
 
+// require_once (__DIR__ . "/../Lib/constants.php");
+// require_once (__DIR__ . "/../Lib/env.php");
+// require_once (__DIR__ . "/../Lib/autoload.php");
+// require_once (__DIR__ . "/../Routes/web.php");
+// use Lib\Http\Route;
 
-require_once (__DIR__ . "/../Lib/constants.php");
-require_once (__DIR__ . "/../Lib/env.php");
-require_once (__DIR__ . "/../Lib/autoload.php");
-require_once (__DIR__ . "/../Routes/web.php");
-use Lib\Http\Route;
-
-Route::run();
+// Route::run();
